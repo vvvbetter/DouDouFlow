4920dde027d9c847a0608e40077d95e89da09fcb
vvvbetter <378638540@qq.com>
2020-12-24 16:51:36 +0800

    工程初始化

---
+++ src/main/java/org/doudou/doudouflow/DoudouflowApplication.java
@@ -0,0 +1,13 @@
+package org.doudou.doudouflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoudouflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoudouflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/doudou/doudouflow/DoudouflowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.doudou.doudouflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoudouflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
